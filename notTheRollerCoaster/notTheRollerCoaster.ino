30db45ac02be3771b23f972cd7add41f9c2733b5
Aidan <asgalb15@gmail.com>
2017-10-20 20:43:42 +0000

    Added Code

--- notTheRollerCoaster/notTheRollerCoaster.ino
+++ notTheRollerCoaster/notTheRollerCoaster.ino
@@ -1,24 +1,28 @@
+#include <Servo.h>
+
 /*
   PhotoMotor
   When a photoresistor registers the passing of a ball, adjusts motor speed based on time.
  */
 
-const int photoStart = 2;    //Photoresistor at the beginning of the device
-const int photoBelt = 3;     //Photoresistor at the beginning of the belt
-const int photoBeltM = 4;    //Photoresistor at the midpoint of the belt
-const int failSafe = 5;      //Photoresistor at the very end, just in case
-const int pwm = 6;           //Pulse Width Modulation, for motors
-const int in_1 = 7;          //Motor input positive
-const int in_2 = 8;          //Motor input ground
+#define photoStart 2    //Photoresistor at the beginning of the device
+#define photoBelt 3     //Photoresistor at the beginning of the belt
+#define photoBeltM 4    //Photoresistor at the midpoint of the belt
+#define failSafe 5      //Photoresistor at the very end, just in case
+#define victor 6        //Input for the victor signal line.
+
 const double ball = 1;       //CHANGE VALUE OF BALL ON PHOTORESISTORS
 const double lengthStart = 3;//Total length of device
 const double lengthBelt = 2; //Length of the belt
+const double radius = 2.8    //Radius of the Wheel
 
 int start;
 int photoRead;
 int timeLeft;
 int beltSpeed;
-int beltVoltage;
+int writeValue;
+
+Servo victor;
 
 
 void setup() {
@@ -26,9 +30,7 @@ void setup() {
   pinMode(photoBelt, INPUT);
   pinMode(photoBeltM, INPUT);
   pinMode(failSafe, INPUT);
-//  pinMode(pwm, OUTPUT);
-  pinMode(in_1, OUTPUT);
-  pinMode(in_2, OUTPUT);
+  victor.attach(victor);
 }
 
 int main() {
@@ -41,6 +43,6 @@ int main() {
 
 void physicsMath(int length, int time) {
   beltSpeed = length/time;
-  beltVoltage = 1; //PUT MATH HERE
+  writeValue = 1; //PUT MATH HERE
 }
   
